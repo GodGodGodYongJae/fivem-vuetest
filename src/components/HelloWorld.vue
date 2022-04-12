95cca85461498de5b4c78219f44c29a0fd905187
kyeonghoon <zhaqj12@naver.com>
2022-04-12 09:38:01 +0900

    vue 초기셋팅

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <p>Hello {{ value }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  names: 'hello-world',
+  value: 'hello-world',
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped></style>
